f6465e3bd03f3d021b3fd7a9ce343aee6f6e1082
sathishnaathi <67456355+sathishnaathi@users.noreply.github.com>
2025-09-07 22:27:23 +0530

    Update setup_repo.py

--- scripts/setup_repo.py
+++ scripts/setup_repo.py
@@ -6,7 +6,7 @@ import requests
 GITHUB_TOKEN = os.getenv("REPO_TOKEN")          # Updated token environment variable
 GITHUB_USERNAME = os.getenv("REPO_USER")        # Updated username environment variable
 GITHUB_ORG = os.getenv("REPO_USER")
-REPO_NAME = "DummyCG_Repo"
+REPO_NAME = "CGDummy_Repo"
 FEATURE_BRANCH = "Feature/CICDAutomation"
 ENVIRONMENT_NAME = "UAT-PROD"
 
