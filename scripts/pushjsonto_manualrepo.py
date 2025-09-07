ceb1d54a39072bf4e0f801af185d28626fb171ce
sathishnaathi <67456355+sathishnaathi@users.noreply.github.com>
2025-09-07 18:45:03 +0530

    Update pushjsonto_manualrepo.py

--- scripts/pushjsonto_manualrepo.py
+++ scripts/pushjsonto_manualrepo.py
@@ -1,17 +1,11 @@
-import os
-import requests
+import os, requests
 
 GITHUB_TOKEN = os.getenv("REPO_TOKEN")
-REPO_OWNER = os.getenv("REPO_OWNER")
-REPO_NAME = "SathishDummy-Repo"
-
-url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}"
-
 headers = {
-    "Authorization": f"Bearer {GITHUB_TOKEN}",
-    "Accept": "application/vnd.github+json",
+  "Authorization": f"Bearer {GITHUB_TOKEN}",
+  "Accept": "application/vnd.github+json",
 }
 
-response = requests.get(url, headers=headers)
-print(f"Status code: {response.status_code}")
-print(response.json())
+resp = requests.get("https://api.github.com/user", headers=headers)
+print("Status:", resp.status_code, resp.json())
+
